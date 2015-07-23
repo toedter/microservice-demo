1ddb9267faa7cb25b1b259a31a560c6e99516743
Kai Toedter <kai.toedter@siemens.com>
2015-07-23 16:57:40 +0200

    better configuration

--- services/user-service/src/main/java/com/toedter/msd/userservice/UserService.java
+++ services/user-service/src/main/java/com/toedter/msd/userservice/UserService.java
@@ -3,11 +3,12 @@ package com.toedter.msd.userservice;
 import com.toedter.msd.userservice.user.UserTestDataLoader;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
-@EnableEurekaClient
+@EnableDiscoveryClient
 public class UserService {
 
     @Bean(initMethod = "loadData")
